963b0ecea73c8da90550f7deb2829530b40c7ac6
gbrine123 <garysnow-brine@outlook.com>
2016-12-21 22:35:21 -0500

    Algorithms - Sorting - Insertion Sort - Part 2
    Initial check-in

---
+++ Algorithms/Sorting/Insertion Sort - Part 2/Solution/src/solution/Solution.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package solution;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author gary.snow-brine
+ */
+public class Solution {
+
+    public static void insertionSortPart2(int[] ar) {
+        for (int i = 1; i < ar.length; i++) {
+            int elem = ar[i];
+            int j = 0;
+            for (j = i - 1; j >= 0; j--) {
+                if (ar[j] > elem) {
+                    ar[j + 1] = ar[j];
+                } else {
+                    ar[j + 1] = elem;
+                    break;
+                }
+            }
+            if (j < 0) {
+                ar[0] = elem;
+            }
+            printArray(ar);
+        }
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int s = in.nextInt();
+        int[] ar = new int[s];
+        for (int i = 0; i < s; i++) {
+            ar[i] = in.nextInt();
+        }
+        insertionSortPart2(ar);
+    }
+    
+    private static void printArray(int[] ar) {
+        for (int n : ar) {
+            System.out.print(n + " ");
+        }
+        System.out.println();
+    }
+    
+}
